50a76b4c40be9de0f95131545115d79c8f05ea85
LlambiasMBP <llambias@live.com>
2026-08-17 14:31:59 +0200

    test changes

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import argparse
+import hydra
 from utils.test_utils import test_model
+from omegaconf import DictConfig
+
+
+@hydra.main(
+    config_path="./confs/training_confs",
+    config_name="default",
+    version_base="1.2",
+)
+def main(cfg: DictConfig) -> None:
+    test_model(cfg.model_dir, batch_size=cfg.get("batch_size", 128))
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train preterm prediction model")
-    parser.add_argument(
-        "save_path",
-        help="Full path to training config YAML (info.name sets the run folder)",
-    )
-    args = parser.parse_args()
-    test_model(args.save_path)
+    main()
